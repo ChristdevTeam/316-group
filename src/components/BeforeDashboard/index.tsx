050485d2fcd526dfb875d8c35042d6fde933a931
christdevtech <michael@christdev.com>
2024-12-04 14:06:26 +0100

    added back seeding

--- src/components/BeforeDashboard/index.tsx
+++ src/components/BeforeDashboard/index.tsx
@@ -1,7 +1,7 @@
 import { Banner } from '@payloadcms/ui/elements/Banner'
 import React from 'react'
 
-// import { SeedButton } from './SeedButton'
+import { SeedButton } from './SeedButton'
 import './index.scss'
 
 const baseClass = 'before-dashboard'
@@ -12,15 +12,19 @@ const BeforeDashboard: React.FC = () => {
       <Banner className={`${baseClass}__banner`} type="success">
         <h4>Welcome to your dashboard!</h4>
       </Banner>
+
+      <p>
+        <SeedButton />
+        {' with a few pages, posts, and projects to jump-start your new site, then '}
+        <a href="/" target="_blank">
+          visit your website
+        </a>
+        {' to see the results.'}
+      </p>
       {/* Here&apos;s what to do next:
       <ul className={`${baseClass}__instructions`}>
         <li>
-          <SeedButton />
-          {' with a few pages, posts, and projects to jump-start your new site, then '}
-          <a href="/" target="_blank">
-            visit your website
-          </a>
-          {' to see the results.'}
+         
         </li>
         <li>
           If you created this repo using Payload Cloud, head over to GitHub and clone it to your
