b56d15e79bd7a82c3146835385b5abf5e68bc8f7
christdevtech <michael@christdev.com>
2025-04-22 18:22:38 +0100

    Fixed the import error

--- src/blocks/SpacingBlock/Component.tsx
+++ src/blocks/SpacingBlock/Component.tsx
@@ -1,5 +1,5 @@
 import { SpacingBlock as SpacingBlockProps } from '@/payload-types'
-import { cn } from '@/utilities/ui'
+import { cn } from '@/utilities/cn'
 
 export const SpacingBlock: React.FC<SpacingBlockProps> = ({ spacing, bgColor, width }) => {
   return <div className={cn(spacing, bgColor, width)} />
