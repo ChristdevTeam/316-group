a54063f5ad3010e7b58311e8bdfc724beaa368d1
christdevtech <michael@christdev.com>
2025-05-21 19:01:05 +0100

    Fixed the slider navigation buttons which were broken on the case study single page

--- src/components/SolutionAndOutcome/index.tsx
+++ src/components/SolutionAndOutcome/index.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { CaseStudy } from '@/payload-types'
-import React, { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import RichText from '../RichText'
 import { Media } from '../Media'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, EffectCoverflow, Autoplay } from 'swiper/modules'
+import type SwiperType from 'swiper'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/effect-coverflow'
@@ -13,15 +14,16 @@ const SolutionAndOutcome: React.FC<{ data: CaseStudy['solutionAndOutcome'] }> = 
   const { content, slider } = data || {}
   const navigationPrevRef = useRef<HTMLDivElement>(null)
   const navigationNextRef = useRef<HTMLDivElement>(null)
+  const [swiper, setSwiper] = useState<SwiperType | null>(null)
 
   return (
-    <div className="container max-w-screen-2xl py-16 lg:py-24">
-      <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-4 lg:mb-8">
+    <div className="container py-16 max-w-screen-2xl lg:py-24">
+      <h2 className="mb-4 text-2xl font-semibold md:text-3xl lg:text-4xl lg:mb-8">
         Solution and outcome
       </h2>
       {content && (
         <RichText
-          className="text-lg md:text-xl mb-12 lg:mb-16"
+          className="mb-12 text-lg md:text-xl lg:mb-16"
           enableGutter={false}
           content={content}
         />
@@ -51,19 +53,20 @@ const SolutionAndOutcome: React.FC<{ data: CaseStudy['solutionAndOutcome'] }> = 
               delay: 6000,
               disableOnInteraction: false,
             }}
+            onSwiper={setSwiper}
             onBeforeInit={(swiper) => {
               // @ts-ignore
               swiper.params.navigation.prevEl = navigationPrevRef.current
               // @ts-ignore
               swiper.params.navigation.nextEl = navigationNextRef.current
             }}
-            className="w-full bg-blue-50 rounded-xl overflow-hidden"
+            className="overflow-hidden w-full bg-blue-50 rounded-xl"
           >
-            <div className="bg-blue-700 absolute bottom-0 left-0 right-0 h-1/5"></div>
+            <div className="absolute right-0 bottom-0 left-0 h-1/5 bg-blue-700"></div>
             {slider.map((slide, index) => (
               <SwiperSlide key={slide.id || index} className="">
-                {/* <div className="bg-blue-700 absolute bottom-0 left-0 right-0 h-1/5"></div> */}
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-8 h-full">
+                {/* <div className="absolute right-0 bottom-0 left-0 h-1/5 bg-blue-700"></div> */}
+                <div className="grid grid-cols-1 gap-8 h-full md:grid-cols-2">
                   <div className="order-2 md:order-none relative h-[350px] md:h-full mb-8 pt-0 md:pt-8">
                     {slide.image && (
                       <Media
@@ -76,7 +79,7 @@ const SolutionAndOutcome: React.FC<{ data: CaseStudy['solutionAndOutcome'] }> = 
                   <div className="flex flex-col justify-center p-8 md:p-none">
                     {slide.title && (
                       <h3
-                        className="text-xl md:text-2xl lg:text-3xl font-semibold mb-4"
+                        className="mb-4 text-xl font-semibold md:text-2xl lg:text-3xl"
                         dangerouslySetInnerHTML={{ __html: slide.title }}
                       ></h3>
                     )}
@@ -89,10 +92,11 @@ const SolutionAndOutcome: React.FC<{ data: CaseStudy['solutionAndOutcome'] }> = 
             ))}
           </Swiper>
 
-          <div className="flex gap-4 absolute bottom-8 right-8 lg:right-24 z-10 slider-navigation">
+          <div className="flex absolute right-8 bottom-8 z-10 gap-4 lg:right-24 slider-navigation">
             <div
               ref={navigationPrevRef}
-              className="w-12 h-12 flex items-center justify-center bg-white rounded-full cursor-pointer shadow-md hover:bg-gray-100 transition-colors nav-prev"
+              className="flex justify-center items-center w-12 h-12 bg-white rounded-full shadow-md transition-colors cursor-pointer hover:bg-gray-100 nav-prev"
+              onClick={() => swiper?.slidePrev()}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -110,7 +114,8 @@ const SolutionAndOutcome: React.FC<{ data: CaseStudy['solutionAndOutcome'] }> = 
             </div>
             <div
               ref={navigationNextRef}
-              className="w-12 h-12 flex items-center justify-center bg-white rounded-full cursor-pointer shadow-md hover:bg-gray-100 transition-colors nav-next"
+              className="flex justify-center items-center w-12 h-12 bg-white rounded-full shadow-md transition-colors cursor-pointer hover:bg-gray-100 nav-next"
+              onClick={() => swiper?.slideNext()}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
